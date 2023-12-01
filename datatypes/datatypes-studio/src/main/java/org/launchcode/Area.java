71ec6fdf7824ceaeae66ac0062c209c5467fb4e9
JohnProphet <87950318+JohnProphet@users.noreply.github.com>
2023-11-30 20:16:06 -0600

    Class 1 Studio

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -7,12 +7,11 @@ public class Area
     public static void main(String[] args)
     {
         double radius;
-        double pi = 3.14;
         Scanner input;
         input = new Scanner(System.in);
         System.out.println("Enter the radius of the circle: ");
         radius = input.nextDouble();
-
+//        while (boolean isNumber = radius instanceof Integer) {;
 
         input.close();
         double area = Circle.getArea(radius);
